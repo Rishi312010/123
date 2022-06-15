7a23790d4ed4b98536d18ecaf9ccd9ee04029a1d
Rishi312010 <104554473+Rishi312010@users.noreply.github.com>
2022-06-15 17:30:43 +0530

    Add files via upload

--- bottle.js
+++ bottle.js
@@ -38,7 +38,7 @@ function draw(){
             text(object[i].label + " " + percent + "%" , object[i].x +15 , object[i].y +15);
             noFill();
             stroke("#FF0000");
-            rect(object[i].x , object[i].y , object[i],width , objects[i].height);
+            rect(object[i].x , object[i].y , object[i],width , object[i].height);
         }
     }
     }
